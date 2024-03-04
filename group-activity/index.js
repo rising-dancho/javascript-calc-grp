4ec4c4d4b670c2adab2a817e33b2c6402581e2fb
Jose Alejo Perez Jr <43742265+rising-dancho@users.noreply.github.com>
2024-03-04 20:17:03 +0800

    feat: added sub on the switch case

--- group-activity/index.js
+++ group-activity/index.js
@@ -2,5 +2,7 @@ function calculate(operator, num1, num2) {
     switch(operator) {
         case 'add':
             return num1 + num2;
+        case 'sub':
+            return num1 - num2;
     }
 }
