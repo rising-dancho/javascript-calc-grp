b02af7474a6d7357cd8ecebb4271234cab4c0fbd
adfinem <43742265+rising-dancho@users.noreply.github.com>
2024-03-04 20:21:33 +0800

    feat: added sub on the switch case (#1)
    Co-authored-by: Godfrey Anthony T. Serojales <34874072+recyclecodes@users.noreply.github.com>

--- group-activity/index.js
+++ group-activity/index.js
@@ -1,11 +1,15 @@
 function calculate(operator, num1, num2) {
-  switch (operator) {
-    case 'add':
-      return num1 + num2;
-  }
+    switch(operator) {
+        case 'add':
+            return num1 + num2;
+        case 'sub':
+            return num1 - num2;
+    }
 }
+
 
 console.log('Addition:', calculate('add', 5, 3));
 console.log('Subtraction:', calculate('subtract', 10, 4));
 console.log('Multiplication:', calculate('multiply', 2, 6));
 console.log('Division:', calculate('divide', 8, 2));
+
