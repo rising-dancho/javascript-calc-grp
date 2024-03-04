bd0e4366dfb8916ef2f5fce0423b41406b9f8fd3
Godfrey Anthony T. Serojales <gaserojales@gmail.com>
2024-03-04 20:23:56 +0800

    four operatons

--- group-activity/index.js
+++ group-activity/index.js
@@ -1,15 +1,23 @@
 function calculate(operator, num1, num2) {
-    switch(operator) {
-        case 'add':
-            return num1 + num2;
-        case 'sub':
-            return num1 - num2;
-    }
+  switch (operator) {
+    case 'add':
+      return num1 + num2;
+    case 'sub':
+      return num1 - num2;
+    case 'multiply':
+      return num1 * num2;
+    case 'divide':
+      if (num2 !== 0) {
+        return num1 / num2;
+      } else {
+        return 'Cannot divide by zero!';
+      }
+    default:
+      return 'Invalid operation!';
+  }
 }
-
 
 console.log('Addition:', calculate('add', 5, 3));
 console.log('Subtraction:', calculate('subtract', 10, 4));
 console.log('Multiplication:', calculate('multiply', 2, 6));
 console.log('Division:', calculate('divide', 8, 2));
-
